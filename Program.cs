5a090ada25ef9910706e676efa615ddab1017e0d
roma7barinov <roma7barinov@mail.ru>
2023-05-23 18:38:32 +0800

    create fiel

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача №2
+// Console.WriteLine("Введите два чсила");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int max = 0;
+// if (num1 > num2) {
+//     max = num1;
+// } else {
+//     max = num2;
+// }
+// Console.WriteLine("Максимальное число = " + max);
+//_________________________
+// Задача №4
+// Console.WriteLine("Введите три чсила");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = 0;
+// if (num1 > num2) {
+//     if (num1 > num3){
+//         max = num1;
+//     } else {
+//         max = num3;
+//     }
+// } else {
+//     if (num2 > num3) {
+//         max = num2;
+//     } else{
+//         max = num3;
+//     }
+// }
+// Console.WriteLine("Максимальное число = " + max);
+//_________________________
+// Задача №6
+// Console.WriteLine("Введите чиcло");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num % 2 == 0 ) {
+//     Console.WriteLine("Четное");
+// } else {
+//     Console.WriteLine("Нечетное");
+// }
+//_________________________
+// Задача №8
+// Console.WriteLine("Введите чиcло");
+//  int num = Convert.ToInt32(Console.ReadLine());
+//  int count = 1;
+// while(num >= count) {
+//     if (count % 2 == 0) {
+//         Console.WriteLine(count);
+//     }
+//     count++;
+// }
